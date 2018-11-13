f46cad4bb8b9e9d0721843fe98ddba7e1df183aa
jian.yao <jian.yao@asiainnovations.com>
2018-11-13 17:47:23 +0800

    spring cloud config on git

--- angle-config-server/src/main/java/com/angle/config/server/ConfigServerApplication.java
+++ angle-config-server/src/main/java/com/angle/config/server/ConfigServerApplication.java
@@ -2,15 +2,15 @@ package com.angle.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author YJ
  * @date 2018/11/12 14:55
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
--- angle-user/angle-user-service/src/main/java/com/angle/user/controller/UserController.java
+++ angle-user/angle-user-service/src/main/java/com/angle/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.angle.user.api.UserApi;
 import com.angle.user.dto.UserDto;
 import com.angle.user.util.UserCache;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController implements UserApi {
 
     @Value("${name}")
